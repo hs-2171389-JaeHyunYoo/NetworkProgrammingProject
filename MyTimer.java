238b4a444b9add7c4362245bcfecb11e0b2e3a84
YooJaeHyun <109721661+hs-2171389-JaeHyunYoo@users.noreply.github.com>
2023-12-17 22:37:19 +0900

    Update MyTimer.java

--- MyTimer.java
+++ MyTimer.java
@@ -24,13 +24,23 @@ public class MyTimer extends JFrame {
         Timer timer = new Timer(1000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+            	//타이머가 시작되면 게임 참여자들이 서로 의사소통하며 라이어를 특정할 수 있도록 채팅방 활성화
+            	for(int i=0; i<Client.clientManagement.size(); i++) {
+            		Client.clientManagement.get(i).textField.setEnabled(true);
+            	}
                 display.setText(Integer.toString(time));
                 time--;
 
                 if (time < 0) {
                     ((Timer) e.getSource()).stop();
+                    //타이머가 종료되면 다시 게임 참여자들이 무분별하게 채팅을 하지 못하도록 채팅방 비활성화
+                    for(int i=0; i<Client.clientManagement.size(); i++) {
+                		Client.clientManagement.get(i).textField.setEnabled(false);
+                	}
                     dispose(); // 타이머 종료 후 창 닫기
+                    Vote vote = new Vote(); //투표 시작
                 }
+                
             }
         });
 
